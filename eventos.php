2a707193ea958934e5e501883c4e405ce8434f5a
ITS-Power17 <cogedoresdemadre@gmail.com>
2024-12-04 17:55:39 -0400

    Add files via upload

---
+++ eventos.php
@@ -0,0 +1,146 @@
+<?php
+include_once("header.php");
+?>
+
+
+
+
+<section class="page-section">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Noticias</h2>
+            <h3 class="section-subheading text-muted">Lamborghini.</h3>
+        </div>
+        <ul class="timeline">
+            <li>
+                <div class="timeline-image"><img style="object-fit: cover;" class="rounded-circle " src="assets/img/noticia1.webp" width="100%" height="100%" alt="..." /></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h6>3 Diciembre 2024</h6>
+                        <h4 class="subheading">Lamborghini Temerario meets Europe</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <p class="text-muted">The new High Performance Electrified Vehicle (HPEV) is presented to European clients in a London venue <br><br>
+
+                            The new Lamborghini Temerario was presented to European guests in London during the last week of November, providing a first opportunity for many to meet the Temerario in person following its global debut at the USA’s Monterey Car Week in August 2024.</p>
+                    </div>
+                </div>
+            </li>
+            <li class="timeline-inverted">
+                <div class="timeline-image"><img style="object-fit: cover;" class="rounded-circle " src="assets/img/noticia2.webp" width="100%" height="100%" alt="..." /></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h6>2 Diciembre 2024</h6>
+                        <h4 class="subheading">Lamborghini Day Japan: The Asia Pacific Premiere of Temerario</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <p class="text-muted">130 Lamborghinis took over the streets of Tokyo on Lamborghini Day. The Lamborghini parade concluded at the National Stadium that hosted the 2020 Olympic games in Shinjuku, Tokyo. The stunning background of the National Stadium made the perfect stage for the finale of Lamborghini Day Japan and Asia Pacific Premiere of Temerario . Over 700 Lamborghini owners, media and enthusiasts across Japan, Asia Pacific and European regions gathered to witness the unveiling of Lamborghini’s third and last plug-in hybrid High Performance Electrified Vehicle (HPEV), Temerario. </p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-image"><img style="object-fit: cover;" class="rounded-circle " src="assets/img/noticia3.webp" width="100%" height="100%" alt="..." /></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h6>2 Diciembre 2024</h6>
+                        <h4 class="subheading">Balenciaga | Automobili Lamborghini Fall 25 Collaboration</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <p class="text-muted">Founded by Spanish-born couturier Cristóbal Balenciaga in 1917 and established in Paris in 1937, Balenciaga revolutionized modern couture with innovations in form and technique. Since 2015, under the artistic direction of Demna, the house has continued to redefine luxury through boundary-pushing collections. Today, Balenciaga encompasses women’s and men’s ready-to-wear, accessories, and objets d’art, seamlessly blending its storied heritage with cutting-edge modernity.</p>
+                    </div>
+                </div>
+            </li>
+            <li class="timeline-inverted">
+                <div class="timeline-image"><img style="object-fit: cover;" class="rounded-circle " src="assets/img/noticia4.jpg" width="100%" height="100%" alt="..." /></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h6>18 Noviembre 2024</h6>
+                        <h4 class="subheading">Lamborghini World Finals, Egor Orudzhev is crowned champion</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <p class="text-muted">Egor Orudzhev claimed a dramatic victory in the Lamborghini World Finals at Jerez de la Frontera, winning the Pro title on a tiebreak and overcoming a drivethrough penalty in the last race of the season.</p>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-image"><img style="object-fit: cover;" class="rounded-circle " src="assets/img/noticia5.jpg" width="100%" height="100%" alt="..." /></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h6>11 Noviembre 2024</h6>
+                        <h4 class="subheading">Revuelto Opera Unica at Lamborghini Esperienza Arte Shanghai</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <p class="text-muted">Automobili Lamborghini unveiled Revuelto Opera Unica, during the Lamborghini Esperienza Arte event in Shanghai. Its combination of craftsmanship and innovation makes this extraordinary vehicle express the artistry of Centro Stile Lamborghini and the possibilities the Ad Personam customization program offers. With a transition of colors from Nero Pegaso to Rosso Efesto symbolizing speed and power, the exterior paintwork demanded 480 hours of handcrafted work. The elegant interiors in Nero Ade and Rosso Efesto include unique details like a carbon nameplate bearing the wording “Opera Unica” in Bianco Monocerus.</p>
+                    </div>
+                </div>
+            </li>
+
+        </ul>
+    </div>
+</section>
+
+
+
+
+
+
+<section class="page-section bg-light">
+    <div class="container">
+        <div class="text-center">
+            <h2 class="section-heading text-uppercase">Eventos</h2>
+            <h3 class="section-subheading text-muted">Los mejores enventos de lamborghini.</h3>
+        </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="team-member">
+                    <img style="object-fit: cover;" class="mx-auto rounded-circle" src="assets/img/evento1.jpeg" alt="..." />
+                    <h4>Supercars & Espresso</h4>
+                    <p class="text-muted">September 14 @ 8:00 am - 5:00 pm
+                    </p>
+
+
+                    <a class="btn btn-dark btn-social mx-2" href="https://swfl.life/event/supercars-espresso/2024-09-14/" aria-label="Parveen Anand LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" style="object-fit: cover;" src="assets/img/Huracán.webp" alt="..." />
+                    <h4>Diana Petersen</h4>
+                    <p class="text-muted">Friday 11/04/2025</p>
+
+                    <a class="btn btn-dark btn-social mx-2" href="https://www.trackdays.co.uk/book/4398/23089/" aria-label="Diana Petersen LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" style="object-fit: cover;" src="assets/img/evento3.webp" alt="..." />
+                    <h4>Lamborghini World Finals</h4>
+                    <p class="text-muted">21 November 2024</p>
+
+                    <a class="btn btn-dark btn-social mx-2" href="https://www.lamborghini.com/en-en/motorsport/news/lamborghini-world-finals-over-10000-spectators-celebrate-the-super-trofeo-champions" aria-label="Larry Parker LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-8 mx-auto text-center">
+                <p class="large text-muted">Descubre la emoción y el lujo en nuestros exclusivos eventos de Lamborghini.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include_once("footer.php");
+?>
